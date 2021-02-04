5a350a5813d2758e13a509455cc1cfb1e587a798
Usuario <Usuario@DESKTOP-5U9J2GL>
2021-02-04 10:57:44 +0100

    Commit controlador cartas

--- Blackjack_AntonioMaeso_Alberto/src/Controlador/ControladorCartas.java
+++ Blackjack_AntonioMaeso_Alberto/src/Controlador/ControladorCartas.java
@@ -1,7 +1,7 @@
 package Controlador;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+
 import java.util.List;
 
 import Modelo.Carta;
@@ -60,12 +60,14 @@ public class ControladorCartas {
 			carta.setRuta("img/"+i+paloTreboles+".jpg");
 			cartas.add(carta);
 		}
-		
-		
 		return cartas;
 		
 	}
-	
+	/**
+	 * Sacamos una carta aleatoria de la baraja y la eliminamos para que no salga mas veces
+	 * @param cartas
+	 * @return
+	 */
 	public Carta getCarta(List<Carta> cartas) {
 		Carta carta= null;
 		int numero =(int)(52 * Math.random());
@@ -78,13 +80,35 @@ public class ControladorCartas {
 		}
 		return carta;
 	}
-	
+
 	
 	public static void main(String[]args) {
 		ControladorCartas cc = new ControladorCartas();
-		List<Carta> cartas = cc.creacion();
-		Carta carta = cc.getCarta(cartas);
-		System.out.println(carta);	
+
+		int contador=0;
+		boolean pedir = true;
+		
+		while(pedir) {
+			List<Carta> cartas = cc.creacion();
+			Carta carta = cc.getCarta(cartas);
+			contador += carta.getValor();
+
+			System.out.println(carta.getNombre());
+			if(contador >= 17 && contador < 21) {
+				System.out.println(contador);
+				System.out.println("me planto");
+				pedir = false;
+				}else if(contador == 21) {
+				System.out.println("Jodeos he ganadooooooo");
+					}else if(contador > 21) {
+						System.out.println(contador);
+						System.out.println("No me jodas he perdido");
+						pedir = false;
+						}else {
+							System.out.println("Dame otra");
+							
+						}
+			}
 	}
 
 }
