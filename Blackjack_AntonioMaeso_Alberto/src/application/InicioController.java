758fefd0aa6ca1daed1d1c846bac346a82c30b72
Usuario <Usuario@DESKTOP-5U9J2GL>
2021-02-04 12:05:25 +0100

    cambio inicio

--- Blackjack_AntonioMaeso_Alberto/src/application/InicioController.java
+++ Blackjack_AntonioMaeso_Alberto/src/application/InicioController.java
@@ -2,18 +2,23 @@ package application;
 
 import java.io.IOException;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Button;
+//import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class InicioController {
+	@FXML
+	private Button btnComienza;
 
 	@FXML
-	private void cambiarEscena (MouseEvent e) {
+	private void cambiarEscena (ActionEvent event) {
 
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("Sample.fxml"));
@@ -24,7 +29,12 @@ public class InicioController {
 			stage.initModality(Modality.APPLICATION_MODAL);
 			stage.setScene(scene);
 			stage.showAndWait();
-			stage.close();
+			
+			
+			Node source = (Node) event.getSource();
+			Stage cierre = (Stage) this.btnComienza.getScene().getWindow(); 
+		    cierre.close();
+
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
